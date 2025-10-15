22c4952ccee83f493d307a4f644ff1ed741603f5
NathanaelRR <nathanael.r.r@gmail.com>
2025-10-15 15:40:22 +0200

    ok

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,24 +1,18 @@
-// import './bootstrap';
-// import React from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
-import AppLoader from './AppLoader'; // 👈 ton composant loader
-import App from './App'; // 👈 ton composant principal
+import AppLoader from './AppLoader';
+import App from './App';
 
 const container = document.getElementById('app');
 
 if (container) {
   const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <AppLoader minDisplayMs={500} />
+      <App />
+    </React.StrictMode>
+  );
 
-//   root.render(
-//     <React.StrictMode>
-//       <AppLoader minDisplayMs={500} />
-//       <App />
-//     </React.StrictMode>
-//   );
-
-root.render(App());
-
-
-  // Quand ton app est prête, cache le loader
   window.finishAppLoader();
 }

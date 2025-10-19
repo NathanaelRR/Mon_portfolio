ab8585b08af4ee0ab4b868bb5dd872da551cc321
NathanaelRR <nathanael.r.r@gmail.com>
2025-10-19 15:57:01 +0200

    ok

--- resources/views/projets/show.blade.php
+++ resources/views/projets/show.blade.php
@@ -18,9 +18,9 @@
                     <div class="image-section" id="imageCarousel">
                         @foreach($projet->images as $image)
                             <figure class="image-item">
-                                {{-- <img src="{{ asset('storage/' . $image->path) }}" alt="Image du projet"> --}}
+                                <img src="{{ asset('storage/' . $image->path) }}" alt="Image du projet">
                                 {{-- <img src="{{ asset('storage/projets/' . $image->path) }}" alt="Image du projet"> --}}
-                                <img src="{{ Storage::disk('s3')->url($image->path) }}" alt="Image du projet">
+                                {{-- <img src="{{ Storage::disk('s3')->url($image->path) }}" alt="Image du projet"> --}}
                             </figure>
                         @endforeach
                     </div>

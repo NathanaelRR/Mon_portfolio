e758daebda7a5f70cb4c8f43f3bc7754b7afe8db
NathanaelRR <nathanael.r.r@gmail.com>
2025-10-19 15:46:14 +0200

    ok

--- app/Http/Controllers/ProjetController.php
+++ app/Http/Controllers/ProjetController.php
@@ -165,11 +165,26 @@ class ProjetController extends Controller
         }
     }
 
+    // private function storeImages(Projet $projet, Request $request)
+    // {
+    //     if ($request->hasFile('images')) {
+    //         foreach ($request->file('images') as $index => $file) {
+    //             $path = $file->store('projets', 'public');
+    //             $projet->images()->create([
+    //                 'path' => $path,
+    //                 'ordre' => $index,
+    //             ]);
+    //         }
+    //     }
+    // }
+
     private function storeImages(Projet $projet, Request $request)
     {
         if ($request->hasFile('images')) {
             foreach ($request->file('images') as $index => $file) {
-                $path = $file->store('projets', 'public');
+                // Upload sur S3 au lieu de public
+                $path = $file->store('projets', 's3');
+
                 $projet->images()->create([
                     'path' => $path,
                     'ordre' => $index,
@@ -178,13 +193,28 @@ class ProjetController extends Controller
         }
     }
 
-     private function deleteImages(Projet $projet, Request $request)
+
+    //  private function deleteImages(Projet $projet, Request $request)
+    // {
+    //     if (!empty($request->delete_images)) {
+    //         foreach ($request->delete_images as $imageId) {
+    //             $image = $projet->images()->find($imageId);
+    //             if ($image) {
+    //                 Storage::disk('public')->delete($image->path);
+    //                 $image->delete();
+    //             }
+    //         }
+    //     }
+    // }
+
+    private function deleteImages(Projet $projet, Request $request)
     {
         if (!empty($request->delete_images)) {
             foreach ($request->delete_images as $imageId) {
                 $image = $projet->images()->find($imageId);
                 if ($image) {
-                    Storage::disk('public')->delete($image->path);
+                    // Supprimer depuis S3
+                    Storage::disk('s3')->delete($image->path);
                     $image->delete();
                 }
             }

ceb100144131bfe5d1f58a8581b49a6b571cbf48
NathanaelRR <nathanael.r.r@gmail.com>
2025-10-19 17:19:37 +0200

    ok

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,23 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProjetController;
 
+// Route::get('/', [ProjetController::class, 'index'])->name('index');
+
+// // Route::resource('projets', ProjetController::class);
+
+// Route::prefix('projets/bo_nthnl_76')->name('projets.')->group(function() {
+//     Route::get('create', [ProjetController::class, 'create'])->name('create');
+//     Route::post('/', [ProjetController::class, 'store'])->name('store');
+//     Route::get('{projet}/edit', [ProjetController::class, 'edit'])->name('edit');
+//     Route::put('{projet}', [ProjetController::class, 'update'])->name('update');
+//     Route::delete('{projet}', [ProjetController::class, 'destroy'])->name('destroy');
+// });
+
+// Front routes
 Route::get('/', [ProjetController::class, 'index'])->name('index');
+Route::get('projets/{projet}', [ProjetController::class, 'show'])->name('projets.show');
 
-Route::resource('projets', ProjetController::class);
-
-Route::prefix('projets/bo_nthnl_76')->name('projets.')->group(function() {
+Route::prefix('projets/bo_nthnl_76')->name('projets.admin.')->group(function() {
     Route::get('create', [ProjetController::class, 'create'])->name('create');
     Route::post('/', [ProjetController::class, 'store'])->name('store');
     Route::get('{projet}/edit', [ProjetController::class, 'edit'])->name('edit');

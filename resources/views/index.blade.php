e0a18fd64e568732a4b1b58fcb979ded64da7d53
NathanaelRR <nathanael.r.r@gmail.com>
2025-10-26 12:50:38 +0100

    ok

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,46 +9,6 @@
             <p>Je développe une expertise principalement orientée vers le <strong>développement informatique et web</strong>, tout en gardant une ouverture vers les <strong>sciences cognitives et comportementales.</strong></p>
     </section>
 
-    <section id="profil" class="section-projets">
-
-            <div class="col" id="div1">
-                <h3>Engagements & Bénévolats</h3>
-                <ul class="engagements">
-                <li>
-                    <strong>Entraîneur de basket</strong><br>
-                    Sporting Club Gières <em>(Aujourd’hui)</em>
-                </li>
-                <li>
-                    <strong>Inclusion numérique</strong><br>
-                    Emmaüs Connect Grenoble <em>(Février 2024 - Septembre 2025)</em>
-                </li>
-                <li>
-                    <strong>Distribution alimentaire</strong><br>
-                    Comité Solidarité Étudiante – Lyon <em>(Mars 2023 - Juin 2023)</em>
-                </li>
-                <li>
-                    <strong>Aide pédagogique</strong><br>
-                    Entraide Scolaire Amicale – Lyon <em>(Mai 2023 - Août 2023)</em>
-                </li>
-                </ul>
-            </div>
-            <div class="col" id="div2">
-                <h3>Expériences Professionnelles</h3>
-                <ul class="experiences">
-                <li>
-                    <strong>Fundraising – Médecins Sans Frontières</strong><br>
-                    Besançon <em>(Juin 2025)</em><br>
-                    Collecte de fonds et sensibilisation du public, développement de compétences en communication et persuasion.
-                </li>
-                <li>
-                    <strong>Employé polyvalent – KFC</strong><br>
-                    Saint-Priest (Lyon) <em>(Juillet - Août 2021)</em><br>
-                    Travail en équipe et gestion des commandes clients.
-                </li>
-                </ul>
-            </div>
-    </section>
-
     <section id="projets" class="section-projets">
 
         <div class="projet-filters">
@@ -89,4 +49,44 @@
             @endforeach
         </div>
     </section>
+
+    {{-- <section id="profil" class="section-projets">
+
+            <div class="col" id="div1">
+                <h3>Engagements & Bénévolats</h3>
+                <ul class="engagements">
+                <li>
+                    <strong>Entraîneur de basket</strong><br>
+                    Sporting Club Gières <em>(Aujourd’hui)</em>
+                </li>
+                <li>
+                    <strong>Inclusion numérique</strong><br>
+                    Emmaüs Connect Grenoble <em>(Février 2024 - Septembre 2025)</em>
+                </li>
+                <li>
+                    <strong>Distribution alimentaire</strong><br>
+                    Comité Solidarité Étudiante – Lyon <em>(Mars 2023 - Juin 2023)</em>
+                </li>
+                <li>
+                    <strong>Aide pédagogique</strong><br>
+                    Entraide Scolaire Amicale – Lyon <em>(Mai 2023 - Août 2023)</em>
+                </li>
+                </ul>
+            </div>
+            <div class="col" id="div2">
+                <h3>Expériences Professionnelles</h3>
+                <ul class="experiences">
+                <li>
+                    <strong>Fundraising – Médecins Sans Frontières</strong><br>
+                    Besançon <em>(Juin 2025)</em><br>
+                    Collecte de fonds et sensibilisation du public, développement de compétences en communication et persuasion.
+                </li>
+                <li>
+                    <strong>Employé polyvalent – KFC</strong><br>
+                    Saint-Priest (Lyon) <em>(Juillet - Août 2021)</em><br>
+                    Travail en équipe et gestion des commandes clients.
+                </li>
+                </ul>
+            </div>
+    </section> --}}
 @endsection
